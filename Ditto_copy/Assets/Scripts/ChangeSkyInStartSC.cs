9ccb0793b612def96845308501027ef7de352adc
DESKTOP-51379VU\metamong <22000127@handong.ac.kr>
2023-10-27 20:34:04 +0900

    스타트맵 계속 수정중..

--- Ditto_copy/Assets/Scripts/ChangeSkyInStartSC.cs
+++ Ditto_copy/Assets/Scripts/ChangeSkyInStartSC.cs
@@ -10,15 +10,6 @@ public class ChangeSkyInStartSC : MonoBehaviour
     private float moveSpeed = 20f;
     [SerializeField] private Transform targetTransform;
 
-    [SerializeField] private GameObject[] Particle1;
-    [SerializeField] private GameObject[] Particle2;
-    [SerializeField] private GameObject[] Particle3;
-    [SerializeField] private GameObject[] Particle4;
-    [SerializeField] private GameObject[] Particle5;
-    [SerializeField] private GameObject[] Particle6;
-    [SerializeField] private GameObject Particle8;
-    [SerializeField] private GameObject Particle9;
-    [SerializeField] private GameObject Particle10;
 
     
 
@@ -27,7 +18,6 @@ public class ChangeSkyInStartSC : MonoBehaviour
     {
         SkyBoxTransform = skyBox.transform;
         StartCoroutine(ChangeSky());
-        StartCoroutine(CineStartFlow());
     }
 
     // Update is called once per frame
@@ -43,28 +33,6 @@ public class ChangeSkyInStartSC : MonoBehaviour
         MoveXRScreenToTargetPosition();  
     }
 
-    private IEnumerator CineStartFlow() 
-    {
-        yield return new WaitForSeconds(18f); 
-         
-        for(int i = 0; i < Particle1.Length; i++)
-        {
-            Particle1[i].SetActive(true);
-        }
-
-        yield return new WaitForSeconds(15f); 
-    for(int i = 0; i < Particle2.Length; i++)
-        {
-            Particle2[i].SetActive(true);
-        }
-
-        yield return new WaitForSeconds(15f); 
-        for(int i = 0; i < Particle3.Length; i++)
-        {
-            Particle3[i].SetActive(true);
-        }
-
-    } 
 
     // XRScreen�� ��ǥ ��ġ�� õõ�� �̵��ϴ� �޼���
     public void MoveXRScreenToTargetPosition()
